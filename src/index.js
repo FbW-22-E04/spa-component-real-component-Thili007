da47e938b77605b75af2184e265030a0273765d7
Thili007 <tpunchihewage@gmail.com>
2022-10-11 15:11:11 +0200

    added solutions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import BootstrapCard from "./Bootstrap-Card/Card";
 
 //create your function here
 
 //remember to use ReactDOM.render to include the component into the website
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BootstrapCard />);
